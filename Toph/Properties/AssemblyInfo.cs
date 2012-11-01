cc23dbd0f3c92db6bd4df77b49a243ba31a5c55c
Rob Tennyson <rob@robtennyson.us>
2012-11-01 13:33:18 -0500

    Added core project

---
+++ Toph/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Toph")]
+[assembly: AssemblyDescription("Core Library")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1bc9f10c-c9dc-42df-9922-eeb43b649059")]
