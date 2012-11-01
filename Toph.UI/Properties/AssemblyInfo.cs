026ba73fb409996c67b2b74c59492876cb3e226f
Rob Tennyson <rob@robtennyson.us>
2012-11-01 13:48:03 -0500

    Added user interface and updated all nuget packages

---
+++ Toph.UI/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Toph.UI")]
+[assembly: AssemblyDescription("User Interface")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a8c03fb4-8292-4f7d-8924-872664bc5249")]
