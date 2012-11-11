913246282d76ea32b5cb165ac08e3031648d7686
Rob Tennyson <rob@robtennyson.us>
2012-11-10 19:17:53 -0600

    Added test project

---
+++ Toph.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Toph.Tests")]
+[assembly: AssemblyDescription("Test Library")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4aaf63ce-0255-4217-9527-c0b3b68058ea")]
