88c108a858541da08eb04da14d45ebb4c2372d59
Rob Tennyson <rob@robtennyson.us>
2013-01-09 09:56:11 -0600

    Updated all nuget packages

---
+++ packages/RobTennyson.Common.1.2.4738.23281/content/Common/ExpressionHelper.cs.pp
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace $rootnamespace$.Common
+{
+    public static class ExpressionHelper
+    {
+        public static Func<T, object> GetSelector<T>(string property)
+        {
+            var param = Expression.Parameter(typeof(T), "item");
+            var prop = Expression.Property(param, property);
+            var expression = Expression.Convert(prop, typeof(object));
+
+            return Expression.Lambda<Func<T, object>>(expression, param).Compile();
+        }
+
+        public static string GetName<T>(Expression<Func<T, object>> propertyExpression)
+        {
+            return propertyExpression.Body.As<MemberExpression>().Member.Name;
+        }
+
+        public static string GetName<T>(this T _, Expression<Func<T, object>> propertyExpression)
+        {
+            return GetName(propertyExpression);
+        }
+    }
+}
